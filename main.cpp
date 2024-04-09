b27391e8dd4d190348d40108fd5afd45efe36e09
dokienvnengine201e4acc1 <159557859+dokienvnengine201e4acc1@users.noreply.github.com>
2024-04-09 15:17:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <windows.h>
 
-int main() {
-  return 0;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    LPCWSTR message = L"Bạn ấn vào Có (Yes) để sửa lỗi.";
+    LPCWSTR title = L"Microsoft Windows";
+
+    int result = MessageBox(NULL, message, title, MB_ICONQUESTION | MB_YESNO);
+
+    if (result == IDYES) {
+        MessageBox(NULL, L"Không thể sửa lỗi được.", title, MB_ICONERROR | MB_OK);
+    }
+    return 0;
 }
