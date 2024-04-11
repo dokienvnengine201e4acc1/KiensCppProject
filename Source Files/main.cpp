337544bfdc48c24d9eebe3afc915e09d43ec0af8
Đỗ Kiên <159557859+dokienvnengine201e4acc1@users.noreply.github.com>
2024-04-11 17:59:34 +0700

    Update main.cpp

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,13 +1,25 @@
 #include <windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    LPCWSTR message = L"Bạn ấn vào Có (Yes) để sửa lỗi.";
-    LPCWSTR title = L"Microsoft Windows";
+	HDC bitblt_hdc = GetDC(0);
+	int bitblt_a = GetSystemMetrics(0);
+	int bitblt_b = GetSystemMetrics(1);
 
-    int result = MessageBox(NULL, message, title, MB_ICONQUESTION | MB_YESNO);
+	LPCWSTR message1 = L"Do you want to do running virus trojan?.";
+	LPCWSTR title1 = L"Trojan.Win32.EffectCpp";
 
-    if (result == IDYES) {
-        MessageBox(NULL, L"Không thể sửa lỗi được.", title, MB_ICONERROR | MB_OK);
-    }
-    return 0;
+	int result1 = MessageBox(NULL, message1, title1, MB_ICONQUESTION | MB_YESNO);
+
+	if (result1 == IDYES) {
+		LPCWSTR message2 = L"Last question. Running trojan? Press any key to continue!";
+		int result2 = MessageBox(NULL, message2, title1, MB_ICONQUESTION | MB_YESNO);
+		if (result2 == IDYES) {
+			MessageBox(NULL, L"Your trojan is running!", title1, MB_ICONERROR | MB_OK);
+			while (true) {
+				BitBlt(bitblt_hdc, 5, 5, bitblt_a, bitblt_b, bitblt_hdc, 0, 0, SRCCOPY);
+				Sleep(100);
+
+			}
+		}
+	}
 }
